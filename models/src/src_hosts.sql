8eb72eae4d9a5bfe13926a177b8208e2f619b395
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-09-20 14:51:40 +0200

    added src_hosts DBT model

---
+++ models/src/src_hosts.sql
@@ -0,0 +1,13 @@
+-- CTE for renaming fields in table RAW_LISTINGS
+WITH raw_hosts AS (
+    SELECT * FROM AIRBNB.RAW.raw_hosts
+)
+SELECT 
+    ID as HOST_ID,
+    NAME,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM
+    raw_hosts    
+    
